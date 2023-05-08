e58a51e661e48415bc96686f0ab2d665cd0212a4
Mustafizur Rahman <rahman.mustafizur@bjitgroup.com>
2023-05-08 16:42:43 +0600

    Add swipper slider in home page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
-<template>
-  <div>    
-    <h1>Index Page</h1>        
+<script lang="ts">
+import { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+    Autoplay
+  },
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+<template>  
+  <div class="home-slider">
+    <swiper
+      :autoplay="autoplay"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper>
   </div>
 </template>
