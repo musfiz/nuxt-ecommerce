4e8a5d52c4879c9e461c0e9ca80d8a68948467b4
Mustafizur Rahman <rahman.mustafizur@bjitgroup.com>
2023-05-09 16:45:48 +0600

    add vue3-carousel for index page slider.

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,45 @@
 <script lang="ts">
-import { Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-
-// Import Swiper styles
-import 'swiper/css';
-
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
 export default {
   components: {
-    Swiper,
-    SwiperSlide,
-    Autoplay
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
   },
   data(){
     return {
-      
+      slider: [
+        {
+          id: 1,
+          url: '1.jpg'
+        },
+        {
+          id: 2,
+          url: '2.jpg'
+        },
+        {
+          id: 3,
+          url: '3.jpg'
+        }
+      ]
     }
   }
 }
 </script>
-<template>  
+<template>
   <div class="home-slider">
-    <swiper
-      :autoplay="autoplay"
-    >
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-    </swiper>
+    <carousel :autoplay="3000" :wrap-around="true" :pause-autoplay-on-hover="true">
+      <slide v-for="item in slider" :key="item.id">
+        <div class="carousel__item">
+          <img :src="`/frontend/slider/${item.url}`" alt="No Image" />
+        </div>
+      </slide>
+      <template #addons>
+        <!-- <Navigation /> -->
+        <!-- <Pagination /> -->
+      </template>
+    </carousel>
   </div>
 </template>
